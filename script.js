460814a66cba3f37545469a39aae4796c284f6cf
spatburgunder <105891060+spatburgunder@users.noreply.github.com>
2026-08-29 17:34:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 (function () {
   "use strict";
+
+//лишнее в боковом меню
+  Lampa.Listener.follow("app", (e) => {
+    if (e.type == "ready") {
+    $("[data-action=anime]").eq(0).remove(); //Аниме
+    }
+  });
+ }
+  if (!window.plugin) startPlugin();
 })();
+
