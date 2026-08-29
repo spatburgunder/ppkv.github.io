59208c96da9462f8782f3df91421137c0b2455d8
spatburgunder <105891060+spatburgunder@users.noreply.github.com>
2026-08-29 17:55:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 (function () {
   "use strict";
 
+  function startPlugin() {
+    window.plugin = true;
+    
 //лишнее в боковом меню
   Lampa.Listener.follow("app", (e) => {
     if (e.type == "ready") {
