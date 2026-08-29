626fbde41099978f21ad1d39d979c9f187510d5a
spatburgunder <105891060+spatburgunder@users.noreply.github.com>
2026-08-29 19:21:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@
   function startPlugin() {
     window.plugin = true;
     
-//лишнее в боковом меню
+
   Lampa.Listener.follow("app", (e) => {
     if (e.type == "ready") {
-    $("[data-action=anime]").eq(0).remove(); //Аниме
+    $("[class=showy-pro-entry-banner]").eq(0).remove(); 
     }
   });
  }
